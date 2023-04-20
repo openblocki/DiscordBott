5fb857ea710416b28bac5b8ce571a561fa16d022
openblocki <damien2006@outlook.de>
2023-04-20 21:39:21 +0200

    Added the ability to join a single specific Voice Channel

---
+++ apps/commands/joinvoice.py
@@ -0,0 +1,24 @@
+import nextcord
+from nextcord.ext import commands
+
+
+class joinvc(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @nextcord.slash_command(
+        name="join",
+        description="Make the Bot join the VC",
+        force_global=True
+    )
+    async def jvc(self, ctx: nextcord.Interaction):
+        channel = nextcord.utils.get(ctx.guild.channels, id=743067601940512888)
+        await nextcord.VoiceClient(self.bot, channel).connect(
+            reconnect=True,
+            timeout=60.0
+        )
+
+
+def setup(bot):
+    bot.add_cog(joinvc(bot))
